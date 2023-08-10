101631a5808857dff709024b9f5ed344e5d54596
Luis Sanchez <luisanchezdh@gmail.com>
2023-08-10 13:52:40 -0400

    added navigation skeleton and bg color

---
+++ smart-face/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navigation from './components/navigation/Navigation'
+import Logo from './components/Logo/Logo'
+
+function App() {
+
+  return (
+    <>
+      <Navigation />
+      <Logo />
+      {/* <ImageLinkForm />
+      <FacialRecognition />  */}
+    </>
+  )
+}
+
+export default App
